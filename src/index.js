c2b951fc4de21097dc7505c3ae8cd98694ffc982
nitarou yamada <nitarouyamada2@gmail.com>
2023-09-04 08:19:36 +0900

    created HomePage component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import Header from './Header';
+import {HomePage} from './HomePage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <HomePage />
   </React.StrictMode>
 );
 
