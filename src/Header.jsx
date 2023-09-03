810bb72abd7cc485a090d0ccb5f65dea11c5ea3b
nitarou yamada <nitarouyamada2@gmail.com>
2023-09-04 08:17:49 +0900

    created Header component

---
+++ src/Header.jsx
@@ -0,0 +1,30 @@
+import HeaderModule from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className={HeaderModule.tab}>
+          1
+          <div className={HeaderModule.dropdown}>
+            1-1
+          </div>
+        </div>
+        <div className={HeaderModule.tab}>
+          2
+          <div className={HeaderModule.dropdown}>
+            2-1
+          </div>
+        </div>
+        <div className={HeaderModule.tab}>
+          3
+          <div className={HeaderModule.dropdown}>
+            3-1
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
